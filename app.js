dc76d37625ff614b49c59259f58a842b9752184a
m3use <m3muse@gmail.com>
2021-05-06 00:58:12 +0800

    welcome pug

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import router from './route.js';
+
 const app = express();
-app.use(router);
+
+app.set('view engine', 'pug');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+app.use(router);
 
 app.listen(8080, () => {
   console.log(`📌 Our Server is listening on PORT 8080`);
--- route.js
+++ route.js
@@ -2,7 +2,20 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  console.log(req);
   res.sendFile(__dirname + '/public/index.html');
 });
 
+router.get('/login', (req, res) => {
+  console.log(req.path);
+  res.render('login.pug');
+});
+
+router.post('/login', (req, res) => {
+  console.log(req.body);
+  res.render('welcome.pug', {
+    data: req.body.id,
+    path: req.path,
+  });
+});
 export default router;
